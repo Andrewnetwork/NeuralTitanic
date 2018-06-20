f4f2f9f66e8867870ae4c696c47f1895431ff575
Andrew Ribeiro <andrewnetwork@gmail.com>
2018-06-20 11:38:37 -0400

    Init

---
+++ src/events.js
@@ -0,0 +1,41 @@
+// callbacks.js 
+// Andrew Riberio - Andrew@kexp.io
+// June 2018
+
+import * as d3 from "d3";
+import {createModel} from './modeling';
+import {gridMenu,tableMenu} from './ui';
+
+export function makeGrid(data){
+    gridMenu(data);
+
+    d3.select("#dat").selectAll("*").remove();
+    var container = d3.select("#dat").append("div");
+    
+    container.selectAll("div").data(data).enter()
+    .append("div").attr("class","gridCell")
+    .text(function (d){return Object.values(d.d)[0];});
+}
+
+export function makeTable(data){
+    tableMenu();
+
+    d3.select("#dat").selectAll("*").remove();
+
+    // Create Table 
+    var table = d3.select("#dat").append("table").attr("class","table");
+  
+    // Create Table Header
+    table.append("thead").append("tr")
+    .selectAll('th').data(data.columns).enter()
+    .append("th").text(function (column) { return column; });
+  
+    // Populate Table
+    table.append("tbody").selectAll("tr").data(data).enter().append("tr")
+    .selectAll("td").data(function (d){return Object.values(d.d);}).enter().append("td")
+    .text(function(d){return d}).exit();
+}
+
+export function startTraining(data){
+    const model = createModel();
+}
